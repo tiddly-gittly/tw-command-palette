e06f3aa007bc920bc83530ecd89f77fdcffc3930
lin onetwo <linonetwo012@gmail.com>
2024-05-12 20:16:30 +0800

    feat: search-story-history

---
+++ src/commandpalette/widgets/build-in-sub-plugins/search-story-history.ts
@@ -0,0 +1,45 @@
+import type { AutocompletePlugin } from '@algolia/autocomplete-js';
+import uniq from 'lodash/uniq';
+import { ITiddlerFields } from 'tiddlywiki';
+
+export const plugin = {
+  getSources() {
+    return [
+      {
+        sourceId: 'story-history',
+        getItems({ query }) {
+          const historyData = $tw.wiki.getTiddlerData<Array<{ title: string }>>('$:/HistoryList') ?? [];
+          const historyTitles = uniq([
+            ...historyData.reverse().map((x) => x.title),
+            ...$tw.wiki.filterTiddlers('[list[$:/StoryList]]'),
+          ]);
+          let filteredHistoryTitles: string[] = [];
+          if ($tw.wiki.getTiddler('$:/plugins/linonetwo/pinyin-fuzzy-search/pinyin-fuzzy-search.js') === undefined) {
+            filteredHistoryTitles = $tw.wiki.filterTiddlers(`[search:title[${query}]]`, undefined, $tw.wiki.makeTiddlerIterator(historyTitles));
+          } else {
+            filteredHistoryTitles = $tw.wiki.filterTiddlers(`[pinyinfuse[${query}]]`, undefined, $tw.wiki.makeTiddlerIterator(historyTitles));
+          }
+          const [first, second, ...rest] = filteredHistoryTitles
+            .map((title) => $tw.wiki.getTiddler(title)?.fields)
+            .filter(Boolean) as ITiddlerFields[];
+          // swap first and second, so its easier to switch to second, like using ctrl + tab in vscode
+          return [second, first, ...rest];
+        },
+        getItemUrl({ item }) {
+          return item.title;
+        },
+        templates: {
+          item({ item }) {
+            if (typeof item.caption === 'string' && item.caption !== '') {
+              return `${item.caption} (${item.title})`;
+            }
+            return item.title;
+          },
+          noResults() {
+            return 'No results.';
+          },
+        },
+      },
+    ];
+  },
+} satisfies AutocompletePlugin<ITiddlerFields, unknown>;
