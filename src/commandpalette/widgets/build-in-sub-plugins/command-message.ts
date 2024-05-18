0c7a6b15efc208d3122b26e67874e295bc248f86
lin onetwo <linonetwo012@gmail.com>
2024-05-19 02:27:23 +0800

    feat: show tiddler name on some message

--- src/commandpalette/widgets/build-in-sub-plugins/command-message.ts
+++ src/commandpalette/widgets/build-in-sub-plugins/command-message.ts
@@ -8,11 +8,14 @@ export const plugin = {
   getSources(parameters) {
     if (parameters.query.length === 0) return [];
     const focusedTiddler = $tw.wiki.getTiddlerText('$:/temp/focussedTiddler');
+    const variables = { currentTiddler: focusedTiddler ?? '' };
+    const { widget } = parameters.state.context as IContext;
     return [
       {
         sourceId: 'message',
         getItems({ query }) {
           if (query === '') return [];
+          // TODO: wikify caption with cache, and search caption with chinese or pinyin
           return $tw.wiki.filterTiddlers(`[all[shadows]tag[$:/tags/CommandPaletteCommand]field:command-palette-type[message]search[${query}]]`)
             .map((title) => $tw.wiki.getTiddler(title)?.fields)
             .filter((tiddler): tiddler is ITiddlerFields => {
@@ -28,7 +31,6 @@ export const plugin = {
         },
         onSelect({ item }) {
           parameters.setContext({ noNavigate: true } satisfies IContext);
-          const { widget } = parameters.state.context as IContext;
           widget?.dispatchEvent?.({
             type: item.text.trim(),
             tiddlerTitle: focusedTiddler,
@@ -38,12 +40,20 @@ export const plugin = {
         },
         templates: {
           header() {
-            return `${lingo('Message')} (${lingo('CurrentTiddler')}: ${focusedTiddler})`;
+            // get rendered caption of focused tiddler
+            let caption = focusedTiddler ? $tw.wiki.getTiddler(focusedTiddler)?.fields?.caption as string | undefined : '';
+            if (caption) {
+              caption = `(${$tw.wiki.renderText('text/plain', 'text/vnd.tiddlywiki', caption, { parentWidget: widget, variables })})`;
+            }
+            // show original title + caption
+            return `${lingo('Message')} - ${lingo('CurrentTiddler')}: ${focusedTiddler} ${caption}`;
           },
           item({ item }) {
             if (typeof item.caption === 'string' && item.caption !== '') {
-              const { widget } = parameters.state.context as IContext;
-              return `${$tw.wiki.renderText('text/plain', 'text/vnd.tiddlywiki', item.caption, { parentWidget: widget })} (${item.title})`;
+              const description = item.description
+                ? ` (${$tw.wiki.renderText('text/plain', 'text/vnd.tiddlywiki', item.description as string, { parentWidget: widget, variables })})`
+                : '';
+              return `${$tw.wiki.renderText('text/plain', 'text/vnd.tiddlywiki', item.caption, { parentWidget: widget, variables })}${description}`;
             }
             return item.title;
           },
