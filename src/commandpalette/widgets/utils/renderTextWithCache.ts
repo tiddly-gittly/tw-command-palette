01aad1e86e7d913dc09d9670970b701acc699ac4
lin onetwo <linonetwo012@gmail.com>
2024-05-20 21:03:21 +0800

    fix: import lingo when render caption
    similar to https://github.com/Jermolene/TiddlyWiki5/pull/8097

--- src/commandpalette/widgets/utils/renderTextWithCache.ts
+++ src/commandpalette/widgets/utils/renderTextWithCache.ts
@@ -1,11 +1,17 @@
 import type { Widget } from 'tiddlywiki';
 
 export function renderTextWithCache(text: unknown, widget: Widget | undefined, variables?: Record<string, string>) {
-  if (text === undefined || typeof text !== 'string' || widget === undefined) return '';
-  return $tw.wiki.getGlobalCache(`wikify-${text}`, () => $tw.wiki.renderText('text/plain', 'text/vnd.tiddlywiki', text, { parentWidget: widget, variables }));
+  if (text === undefined || typeof text !== 'string') return '';
+  return $tw.wiki.getGlobalCache(
+    `wikify-${text}`,
+    () => $tw.wiki.renderText('text/plain', 'text/vnd.tiddlywiki', `\\import [[$:/core/macros/lingo]]\n\n${text}`, { parentWidget: widget, variables }),
+  );
 }
 
 export function renderHTMLWithCache(text: unknown, widget: Widget | undefined, variables?: Record<string, string>) {
-  if (text === undefined || typeof text !== 'string' || widget === undefined) return '';
-  return $tw.wiki.getGlobalCache(`wikify-html-${text}`, () => $tw.wiki.renderText('text/html', 'text/vnd.tiddlywiki', text, { parentWidget: widget, variables }));
+  if (text === undefined || typeof text !== 'string') return '';
+  return $tw.wiki.getGlobalCache(
+    `wikify-html-${text}`,
+    () => $tw.wiki.renderText('text/html', 'text/vnd.tiddlywiki', `\\import [[$:/core/macros/lingo]]\n\n${text}`, { parentWidget: widget, variables }),
+  );
 }
