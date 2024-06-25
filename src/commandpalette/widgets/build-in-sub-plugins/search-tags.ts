c1b164e2417510bb0719788ece228bcd3649f786
lin onetwo <linonetwo012@gmail.com>
2024-06-25 23:11:45 +0800

    feat: now searching completed task, and cal, when search tags
    fixes #32 fixes #31

--- src/commandpalette/widgets/build-in-sub-plugins/search-tags.ts
+++ src/commandpalette/widgets/build-in-sub-plugins/search-tags.ts
@@ -1,6 +1,7 @@
 import type { AutocompletePlugin } from '@algolia/autocomplete-js';
 import { ITiddlerFields } from 'tiddlywiki';
 import { checkIsSearchTags } from '../utils/checkPrefix';
+import { titleTextExclusionFilter } from '../utils/configs';
 import { IContext } from '../utils/context';
 import { debounced } from '../utils/debounce';
 import { filterTiddlersAsync } from '../utils/filterTiddlersAsync';
@@ -13,7 +14,7 @@ export const plugin = {
       return [];
     }
     const onSelect = (item: ITiddlerFields) => {
-      const filter = `[[${item.title}]] [tag[${item.title}]]`;
+      const filter = `[[${item.title}]] [tag[${item.title}]] ${titleTextExclusionFilter()}`;
       parameters.setContext({ newQuery: '', noClose: true, noNavigate: true, filter } satisfies IContext);
     };
     return await debounced([{
