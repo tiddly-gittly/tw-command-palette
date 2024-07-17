9ab39d76e8f874acf0e85a02994be6fd4dc673fe
lin onetwo <linonetwo012@gmail.com>
2024-07-17 14:59:25 +0800

    fix: dark theme detect

--- src/commandpalette/widgets/utils/handleDarkMode.ts
+++ src/commandpalette/widgets/utils/handleDarkMode.ts
@@ -1,10 +1,12 @@
 export function handleDarkMode() {
-  const isDark = $tw.wiki.getTiddlerText('$:/info/darkmode') === 'yes';
-  if (isDark) {
-    // https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-theme-classic/#dark-mode
-    const dataset = document.body?.dataset;
-    if (dataset !== undefined) {
+  const isDark = $tw.wiki.filterTiddlers('[[$:/palette]get[text]get[color-scheme]compare:string:eq[dark]]').length > 0;
+  const dataset = document.body?.dataset;
+  if (dataset !== undefined) {
+    if (isDark) {
+      // https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-theme-classic/#dark-mode
       dataset.theme = 'dark';
+    } else {
+      dataset.theme = 'light';
     }
   }
 }
