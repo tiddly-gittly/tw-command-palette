c1b8d40c3f63c2e738115588b70755df1a45221f
linonetwo <linonetwo012@gmail.com>
2024-09-27 18:27:48 +0800

    fix: recent search not apply to input
    fixes #30

--- src/autocomplete/widgets/build-in-sub-plugins/search-recent.ts
+++ src/autocomplete/widgets/build-in-sub-plugins/search-recent.ts
@@ -1,7 +1,10 @@
-import { StateUpdater } from '@algolia/autocomplete-core';
+import { GetSourcesParams, StateUpdater } from '@algolia/autocomplete-core';
 import type { AutocompletePlugin } from '@algolia/autocomplete-js';
 import { createLocalStorageRecentSearchesPlugin, RecentSearchesPluginData } from '@algolia/autocomplete-plugin-recent-searches';
 import { RecentSearchesItem } from '@algolia/autocomplete-plugin-recent-searches/dist/esm/types';
+import { AutocompleteNavigator } from '@algolia/autocomplete-shared/dist/esm/core/AutocompleteNavigator';
+import { ITiddlerFields } from 'tiddlywiki';
+import { checkIsUnderFilter } from '../utils/checkPrefix';
 import { IContext } from '../utils/context';
 import { getIconSvg } from '../utils/getIconSvg';
 import { lingo } from '../utils/lingo';
@@ -9,17 +12,21 @@ import { lingo } from '../utils/lingo';
 export const plugin = (id: string): AutocompletePlugin<RecentSearchesItem, RecentSearchesPluginData<RecentSearchesItem>> => {
   let setContext: StateUpdater<IContext> | undefined;
   let refresh: () => Promise<void> | undefined;
+  let navigator: AutocompleteNavigator<RecentSearchesItem> | undefined;
   const deleteIcon = getIconSvg('$:/core/images/delete-button', undefined);
   const recentSearchesPlugin = createLocalStorageRecentSearchesPlugin({
     key: `recent-${id}`,
     subscribe(parameters) {
       parameters.setContext?.({ addHistoryItem: (text: string) => recentSearchesPlugin.data?.addItem({ id: text, label: text }) } satisfies IContext);
       setContext = parameters.setContext as unknown as StateUpdater<IContext>;
+      navigator = parameters.navigator;
       refresh = parameters.refresh.bind(parameters);
     },
-    transformSource({ source }) {
-      const onSelect = (text: string) => {
-        setContext?.({ newQuery: text, noClose: true, noNavigate: true } satisfies IContext);
+    transformSource({ source, state }) {
+      const onSelect = (item: RecentSearchesItem) => {
+        const newContext = { newQuery: item.id, noClose: true, noNavigate: true } satisfies IContext;
+        setContext?.(newContext);
+        navigator?.navigate?.({ item, itemUrl: item.id, state: { ...state, context: { ...state.context, ...newContext } } });
       };
       return {
         ...source,
@@ -27,12 +34,12 @@ export const plugin = (id: string): AutocompletePlugin<RecentSearchesItem, Recen
           return item.id;
         },
         async getItems(parameters) {
-          if (parameters.query.length > 0) return [];
+          if (parameters.query.length > 0 || checkIsUnderFilter(parameters as unknown as GetSourcesParams<ITiddlerFields>)) return [];
           const items = source.getItems(parameters);
           return await items;
         },
         onSelect({ item }) {
-          onSelect(item.id);
+          onSelect(item);
         },
         templates: {
           ...source.templates,
@@ -51,10 +58,10 @@ export const plugin = (id: string): AutocompletePlugin<RecentSearchesItem, Recen
               {
                 class: 'tw-commandpalette-search-recent-item',
                 onclick: () => {
-                  onSelect(item.id);
+                  onSelect(item);
                 },
                 onTap: () => {
-                  onSelect(item.id);
+                  onSelect(item);
                 },
               },
               createElement('span', {}, item.id),
