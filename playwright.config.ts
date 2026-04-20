2ca392ab4ab13c02e8d8f59138a5ed3b193d4fad
lin onetwo <linonetwo012@gmail.com>
2026-04-20 14:10:55 +0800

    chore: sync Modern.TiddlyDev standard and bump tiddlywiki-plugin-dev to ^0.5.7 (#43)

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
   reporter: [['list'], ['html', { open: 'never' }]],
   use: {
     baseURL,
-    locale: 'zh-CN',
     trace: 'on-first-retry',
   },
   webServer: {
